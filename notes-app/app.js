040b6f81dcd53e45ba3fb27aca14f43a14e210fa
raphael <raphaelsantosdesigner@gmail.com>
2022-02-09 20:01:13 -0800

    Getting input from users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,19 @@
 const chalk = require("chalk");
 const getNotes = require('./notes.js')
 
-const newNotes = getNotes();
+// const newNotes = getNotes();
 
-console.log(chalk.red(newNotes));
+// console.log(chalk.red(newNotes));
 
+// console.log(process.argv[2])
+
+const command = process.argv[2]
+
+console.log(process.argv)
+
+if(command === 'add'){
+    console.log("adding nodes")
+} else if (command === "remove"){
+    console.log("Removing notes")
+}
 
